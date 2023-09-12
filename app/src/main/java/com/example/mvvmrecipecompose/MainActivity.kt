1591e4db1e694459252d9a9b108d86be3bd593ab
khizzar <muhammadkhizzar21@gmail.com>
2023-09-12 02:06:22 -0700

    row test

--- app/src/main/java/com/example/mvvmrecipecompose/MainActivity.kt
+++ app/src/main/java/com/example/mvvmrecipecompose/MainActivity.kt
@@ -5,14 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -28,24 +32,38 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier.background(color = Color(R.color.light_grey)).fillMaxSize()) {
+            Column(modifier = Modifier
+                .fillMaxSize()) {
                 Image(painter = painterResource(id = R.drawable.happy_meal),
                     contentDescription = "happy meal", contentScale = ContentScale.Crop,
-                    modifier = Modifier.height(300.dp).fillMaxWidth())
+                    modifier = Modifier
+                        .height(300.dp)
+                        .fillMaxWidth())
 
                 Column(modifier = Modifier.padding(16.dp)) {
-                    Text(text = "Happy Meal", style = TextStyle(
-                        fontSize = TextUnit(26f, TextUnitType.Sp)
-                    ))
+                    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                        Text(text = "Happy Meal", style = TextStyle(
+                            fontSize = TextUnit(26f, TextUnitType.Sp)
+                        ))
+
+                        Text(text = "600 Rs", style = TextStyle(
+                            fontSize = TextUnit(17f, TextUnitType.Sp),
+                            color = Color(android.graphics.Color.GREEN)
+                        ), modifier = Modifier.align(Alignment.CenterVertically))
+                    }
+
                     Spacer(modifier = Modifier.padding(top = 10.dp))
+
                     Text(text = "500 Calories", style = TextStyle(
                         fontSize = TextUnit(17f, TextUnitType.Sp)
                     ))
+
                     Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Text(text = "600 Rs", style = TextStyle(
-                        fontSize = TextUnit(17f, TextUnitType.Sp),
-                        color = Color(android.graphics.Color.GREEN)
-                    ))
+
+                    Button(onClick = { /*TODO*/ }, modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                        Text(text = "Order Now")
+                    }
+
                 }
             }
         }
